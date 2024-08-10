aef327147af92b1fae9cfd81a5d9bc0eeb37f068
Jaswant Mandloi <jaswantmandloi@Jaswants-MacBook-Air.local>
2024-08-10 15:54:43 +0530

    write test scenarios for the step number 1

--- __tests__/stringUtils.test.ts
+++ __tests__/stringUtils.test.ts
@@ -1,7 +1,17 @@
+import { getSumFromString } from "@/app/stringUtils";
 
 
 describe("String Utils", () => {
-  it("demo", () => {
-    expect(1).toBe(1);
+  test("getSumFromString should return 0 for empty string", () => {
+    expect(getSumFromString("")).toBe(1);
   });
+
+  test("getSumFromString should return same number if only single number is provided in string", () => {
+    expect(getSumFromString("1")).toBe(1);
+  })
+
+  test("getSumFromString should return sum of numbers in string", () => {
+    expect(getSumFromString("1,2")).toBe(3);
+  })
+
 });
---
+++ src/app/stringUtils.ts
@@ -0,0 +1,3 @@
+export function getSumFromString(stringToBeParsed: string): number {
+  return 0;
+}
