a7b759a82d34a9f6230f22b500eef9f6181c43d4
Jaswant Mandloi <jaswantmandloi@Jaswants-MacBook-Air.local>
2024-08-10 16:02:59 +0530

    refactor getSumFromString to pass the test cases.

--- __tests__/stringUtils.test.ts
+++ __tests__/stringUtils.test.ts
@@ -2,16 +2,21 @@ import { getSumFromString } from "@/app/stringUtils";
 
 
 describe("String Utils", () => {
-  test("getSumFromString should return 0 for empty string", () => {
-    expect(getSumFromString("")).toBe(1);
-  });
 
-  test("getSumFromString should return same number if only single number is provided in string", () => {
-    expect(getSumFromString("1")).toBe(1);
-  })
+  describe("getSumFromString basic functionality", () => {
 
-  test("getSumFromString should return sum of numbers in string", () => {
-    expect(getSumFromString("1,2")).toBe(3);
+    test("getSumFromString should return 0 for empty string", () => {
+      expect(getSumFromString("")).toBe(0);
+    });
+
+    test("getSumFromString should return same number if only single number is provided in string", () => {
+      expect(getSumFromString("1")).toBe(1);
+    })
+
+    test("getSumFromString should return sum of numbers in string", () => {
+      expect(getSumFromString("1,2")).toBe(3);
+    })
+
   })
 
 });
--- src/app/stringUtils.ts
+++ src/app/stringUtils.ts
@@ -1,3 +1,13 @@
 export function getSumFromString(stringToBeParsed: string): number {
-  return 0;
+    if (stringToBeParsed.length === 0) {
+        return 0;
+    }
+    const numbers = stringToBeParsed.split(",");
+    if (numbers.length === 1) {
+        return parseInt(numbers[0]);
+    }
+    const sum = numbers.reduce((acc, curr) => {
+        return acc + parseInt(curr);
+    }, 0);
+    return sum;
 }
